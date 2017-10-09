1d93c6c234e63b5031b2a078f4a2fd4879e7ae72
shivapbhusal <civabhusal@gmail.com>
2017-10-09 10:11:09 -0400

    plus, minus, division, multiplication added

--- EvalExpression.java
+++ EvalExpression.java
@@ -7,7 +7,7 @@
 
 public class EvalExpression {
 	public static void main(String[] args ){
-		String myExpString="10*10"; 
+		String myExpString="10-20"; 
 		float result=evalExp(myExpString); 
 		System.out.println(result); 
 
@@ -16,33 +16,70 @@ public class EvalExpression {
 	public static float evalExp(String expString){
 		int expLength=expString.length();
 		float result=0;
-		String tempResult=""; 
+		String tempResult="";
+		char operator='+';
 		for (int i=0;i<expLength;i++){
 
 			if (expString.charAt(i)=='+'){
-               result=result+Float.parseFloat(tempResult);
+               result=calculate(result,tempResult,operator);
+               operator=expString.charAt(i);
                tempResult="";
 			}
 
 			else if (expString.charAt(i)=='-'){
-               result=result-Float.parseFloat(tempResult);
+               result=calculate(result,tempResult,operator);
+               operator=expString.charAt(i);
                tempResult="";
            }
 
             else if (expString.charAt(i)=='*'){
-            	result=result*Float.parseFloat(tempResult);
-            	tempResult="";
+            	result=calculate(result,tempResult,operator);
+                operator=expString.charAt(i);
+                tempResult="";
             }
             else if (expString.charAt(i)=='/'){
-            	result=result/Float.parseFloat(tempResult);
-            	tempResult="";
+            	result=calculate(result,tempResult,operator);
+                operator=expString.charAt(i);
+                tempResult="";
             }
 			else {
 				tempResult=tempResult+expString.charAt(i);
+				if (i==expLength-1)
+				{
+					result=calculate(result,tempResult,operator);
+					tempResult="";
+
+				}
 			}
 		}
-		result=result+Float.parseFloat(tempResult);
 		return result; 
+	}
+
+	public static float calculate(float result,String tempResult,char operator){
+		if (operator=='+')
+		{
+			result=result+Float.parseFloat(tempResult);
+			return result;
+
+		}
+		else if (operator=='-')
+		{
+			result=result-Float.parseFloat(tempResult);
+			return result;
+
+		}
+		else if (operator=='*')
+		{
+			result=result*Float.parseFloat(tempResult);
+			return result;
+
+		}
+		else 
+		{
+			result=result/Float.parseFloat(tempResult);
+			return result;
+
+		}
 	}
 
 }
